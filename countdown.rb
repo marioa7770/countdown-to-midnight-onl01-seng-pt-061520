f719b61c5c0c6de1bcb176056840876cf863e98b
mario acevedo <jun_acevedo415@yahoo.com>
2020-06-27 19:28:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
-number = 10
- while number > 0
-   puts "#{number} SECOND(S)!"
-   number -= 1
- break if number <= 0
- puts "HAPPY NEW YEAR!"  
-end
-
+def countdown(number)
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
