f672f8a1f9180823fb6afe14af8e070e617b215a
mario acevedo <jun_acevedo415@yahoo.com>
2020-06-27 19:29:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,16 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+def countdown_with_sleep(number)
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+countdown(10)
